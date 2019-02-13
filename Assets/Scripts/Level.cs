aa62f77d5eb4d3882b81ebedc6b85cc339aed546
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-13 17:06:38 +0100

    init + Creat splash screen and start screen

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour {
+
+    [SerializeField] float timeToWait = 4f;
+
+    int currentSceneIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScreenWithDelay());
+        }
+	}
+
+    public void LoadStartScreen()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private IEnumerator LoadStartScreenWithDelay()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadStartScreen();
+    }
+	
+}
