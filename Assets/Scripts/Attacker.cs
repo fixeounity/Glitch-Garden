40f209b82ce74c81d6fcb8ada64f351d1285f0ea
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-16 13:04:43 +0100

    Tune lizard's movement speed while running jump animation by using animation events

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour {
 
-    [Range(0f, 5f)] [SerializeField] float walkSpeed = 1f;
+    [Range(0f,5f)]float currentSpeed = 1f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
 	}
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
 }
