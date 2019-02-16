b5a9ba23815f401b4cb6d26fc046df0e9185a540
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-16 15:00:29 +0100

    Make projectile move

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [Range(0f, 10f)] [SerializeField] float speed = 2f;
+
+    private void Move()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+    }
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,16 +6,13 @@ public class Shooter : MonoBehaviour {
 
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] GameObject gun;
-    [Range(0f, 10f)][SerializeField] float projectileSpeed = 2f;
 	
     public void Fire()
     {
-        var projectile = Instantiate(
+        Instantiate(
             projectilePrefab,
             gun.transform.position,
             projectilePrefab.transform.rotation
             );
-        projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileSpeed, 0f);
-        return;
     }
 }
