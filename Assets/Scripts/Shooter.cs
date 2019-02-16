d18fa8f4c6adf8f2587fd39a0de0d3c63e293c0b
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-16 14:34:33 +0100

    Spawn cactus's projectile

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject gun;
+    [Range(0f, 10f)][SerializeField] float projectileSpeed = 2f;
+	
+    public void Fire()
+    {
+        var projectile = Instantiate(
+            projectilePrefab,
+            gun.transform.position,
+            projectilePrefab.transform.rotation
+            );
+        projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileSpeed, 0f);
+        return;
+    }
+}
