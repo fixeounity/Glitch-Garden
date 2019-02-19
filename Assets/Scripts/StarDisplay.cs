37248249735571d8e88e7dbc11998800ba485d72
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-19 19:07:32 +0100

    Create resource system UI

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StarDisplay : MonoBehaviour {
+
+    [SerializeField] int stars = 100;
+    TextMeshProUGUI starText;
+
+    private void Start()
+    {
+        starText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars < amount) return;
+        stars -= amount;
+        UpdateDisplay();
+    }
+
+}
