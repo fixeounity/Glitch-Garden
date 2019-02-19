5b66ea6e4944898d5166db89314447a24e80269a
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-19 14:41:29 +0100

    Spawn defenders within game grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -9,14 +9,27 @@ public class DefenderSpawner : MonoBehaviour {
 
     private void OnMouseDown()
     {
-        CreateDefender();
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
     }
     
-    private void CreateDefender()
+    private void SpawnDefender(Vector2 roundedPos)
     {
         if (!defenderPrefab) return;
-        var spawn3DPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var spawn2DPosition = new Vector2(spawn3DPosition.x, spawn3DPosition.y);
-        var defender = Instantiate(defenderPrefab, spawn2DPosition, Quaternion.identity);
+        var defender = Instantiate(defenderPrefab, roundedPos, Quaternion.identity);
     }
 }
