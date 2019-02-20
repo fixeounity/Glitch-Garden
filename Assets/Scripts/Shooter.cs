5647bb21bd0f0b23afdfd666868867086434623e
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-20 14:33:50 +0100

    Determine whether an attacker is in a defender's lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,7 +6,25 @@ public class Shooter : MonoBehaviour {
 
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] GameObject gun;
-	
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Shoot");
+        }
+        else
+        {
+            Debug.Log("Sit and wait");
+        }
+    }
+
     public void Fire()
     {
         Instantiate(
@@ -15,4 +33,25 @@ public class Shooter : MonoBehaviour {
             projectilePrefab.transform.rotation
             );
     }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(var spawner in attackerSpawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        var attackerCount = myLaneSpawner.transform.childCount;
+        return attackerCount > 0;
+    }
+
 }
