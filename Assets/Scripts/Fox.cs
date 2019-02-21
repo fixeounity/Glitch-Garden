0ef94f7fdeb0f0875d615a9049629d62fd6d4fab
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-21 11:04:57 +0100

    Create attacker Mr. Fox

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void TriggerAnimation(ref GameObject otherObject)
+    {
+        if (otherObject.GetComponent<Gravestone>())
+        {
+            animator.SetTrigger("jumpTrigger");
+        }
+        else
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+
+        if (!otherObject.GetComponent<Defender>()) return;
+        TriggerAnimation(ref otherObject);
+
+    }
+}
